7478fa26171a4a253cf01f666a41ea5f7b98f9dc
劉家豪 <howard.liu@learningtech.com.tw>
2018-01-25 18:29:55 +0800

    (^) add some features

--- renderer.js
+++ renderer.js
@@ -1,68 +1,41 @@
-const { webFrame } = require('electron');
+const {
+    webFrame
+} = require('electron');
 const ipc = require('electron').ipcRenderer;
 const dialogs = require('dialogs')();
+const Sortable = require('@shopify/draggable/lib/sortable').default;
 
 webFrame.setZoomFactor(1);
-
-$('.add').click(() => dialogs.prompt('url', 'www.youtube.com', (urls) => urls.split(' ').forEach((url) => addWebview(url))));
-$('.import').click(() => ipc.send('import'));
-$('.export').click(() => ipc.send('export', $(".webview").map((i, e) => $(e).attr('src')).get()));
-
-function addWebview(url) {
-    if (url) {
-        const $wrapper = $('.wrapper');
-        const i = $('.panel').length - 1;
-        const webviewId = "webview-" + (i + 1);
-        url = url.startsWith('http://') || url.startsWith('https://') ? url : 'http://'.concat(url);
-        $('<div class="panel">' +
-            '<div class="tools">' +
-            '<div class="remove">' +
-            '<i class="far fa-times-circle"></i>' +
-            '</div>' +
-            '<div class="expand view-control">' +
-            '<i class="fas fa-expand"></i>' +
-            '</div>' +
-            '<div class="collapse view-control">' +
-            '<i class="fas fa-compress"></i>' +
-            '</div>' +
-            '</div>' +
-            '<webview id="' + webviewId + '" class="webview" src="' + url + '" allowpopups webpreferences="nodeIntegration=no,nativeWindowOpen=yes"></webview>' +
-            '</div>')
-            .insertBefore('.panel.last');
-    }
-}
-
-$('.wrapper').on('click', '.remove', removeWebview);
-$('.wrapper').on('click', '.view-control', controlViewScreen);
-
-function removeWebview() {
-    const $this = $(this).closest('.panel');
-    $this.remove();
-}
-
-function controlViewScreen() {
-    const $this = $(this);
-    const $panel = $this.closest('.panel');
-
-    $panel.find('.view-control').show();
-    $this.hide();
-    if ($panel.hasClass('panel-one-full-col')) {
-        $panel.removeClass('panel-one-full-col');
-    } else {
-        $panel.addClass('panel-one-full-col');
-    }
-}
+addWebview('www.youtube.com');
 
 ipc.on('urls', (event, urls) => {
     urls.forEach(url => addWebview(url));
-})
+});
 
-$(".n-col").click(() => {
-    var $full = $('.one-full-col');
-    var $one = $('.one-col');
-    var $two = $('.two-col');
-    var $three = $('.three-col');
-    var $wrapper = $('.wrapper');
+const sortable = new Sortable(document.querySelector('.wrapper'), {
+    draggable: '.wrapper-three-col .panel:not(.panel-template):not(.panel-dashboard)',
+    handle: '.wrapper-three-col .panel:not(.panel-template):not(.panel-dashboard)',
+    classes: {
+        mirror: 'panel-mirror'
+    }
+});
+
+$('.add').click(askForAddingWebview);
+$('.import').click(() => requestToMain('import'));
+$('.export').click(() => requestToMain('export', $(".webview").map((i, e) => $(e).attr('src')).get()));
+$(".n-col").click(toggleWrapperViewStyle);
+$('.reload-all').click(changeReloadSetting);
+
+$('.wrapper').on('click', '.remove', removeWebview);
+$('.wrapper').on('click', '.view-control', controlViewScreen);
+$('.wrapper').on('click', '.nav-control', controlNavigation);
+
+function toggleWrapperViewStyle() {
+    const $full = $('.one-full-col');
+    const $one = $('.one-col');
+    const $two = $('.two-col');
+    const $three = $('.three-col');
+    const $wrapper = $('.wrapper');
     if ($full.is(':visible')) {
         $full.hide();
         $three.show();
@@ -84,4 +57,105 @@ $(".n-col").click(() => {
         $wrapper.removeClass('wrapper-three-col');
         $wrapper.addClass('wrapper-two-col');
     }
-})
+}
+
+function requestToMain(eventName, data) {
+    if (data) {
+        ipc.send(eventName, data);
+    } else {
+        ipc.send(eventName);
+    }
+}
+
+function askForAddingWebview() {
+    dialogs.prompt('url', 'www.youtube.com', (urls) => {
+        if (urls) {
+            urls.split(' ').forEach((url) => addWebview(url));
+        }
+    })
+}
+
+function changeReloadSetting() {
+    const intervalID = $('#intervalID').val();
+    clearInterval(intervalID);
+    const $this = $(this);
+    const $period = $('.period');
+    $period.removeClass('period-on');
+    dialogs.prompt('period (unit: sec, if reload once, no input and [ok])', '', (period_str) => {
+        if (period_str !== undefined) {
+            if (period_str) {
+                $period.addClass('period-on');
+                autoReload(period_str);
+            } else {
+                autoReload();
+            }
+        }
+    })
+}
+
+function autoReload(period_str) {
+    const $intervalHidden = $('#intervalID');
+
+    if (period_str) {
+        const period = parseInt(period_str) * 1000;
+        var id = setInterval(reload, period)
+        $intervalHidden.val(id);
+    } else {
+        reload();
+    }
+}
+
+
+function reload() {
+    const allWebViews = $('.panel:not(.panel-template) webview');
+    $.each(allWebViews, (i, e) => {
+        e.reload();
+    });
+}
+
+function removeWebview() {
+    const $this = $(this).closest('.panel');
+    $this.remove();
+}
+
+function controlViewScreen() {
+    const $this = $(this);
+    const $panel = $this.closest('.panel');
+
+    $panel.find('.view-control').show();
+    $this.hide();
+    if ($panel.hasClass('panel-one-full-col')) {
+        $panel.removeClass('panel-one-full-col');
+    } else {
+        $panel.addClass('panel-one-full-col');
+    }
+}
+
+function controlNavigation() {
+    const $this = $(this);
+    const $panel = $this.closest('.panel');
+    const $webview = $panel.find('webview');
+    const webviewDOM = $webview[0];
+    if ($this.is('.reload')) {
+        webviewDOM.reload();
+    } else if ($this.is('.goback')) {
+        webviewDOM.goBack();
+    } else if ($this.is('.goforward')) {
+        webviewDOM.goForward();
+    }
+}
+
+function addWebview(url) {
+    if (url) {
+        const i = $('.panel:not(.panel-template):not(".panel-dashboard")').length;
+        const webviewId = "webview-" + (i + 1);
+        url = url.startsWith('http://') || url.startsWith('https://') ? url : 'http://'.concat(url);
+
+        const $panel = $('.panel-template').clone().removeClass('panel-template');
+        const $webview = $panel.find('webview');
+        $webview
+            .attr('id', webviewId)
+            .attr('src', url);
+        $panel.insertBefore('.panel-template');
+    }
+}
